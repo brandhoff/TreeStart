b51aca1550173260d82d2b535545e5b4cea1b436
Atinona <antonia.runge@gmail.com>
2024-06-16 21:53:19 +0200

    Added main.cpp and tree.cpp...
    ...as requested in Exercise 1.

---
+++ tree.cpp
@@ -0,0 +1,8 @@
+class tree
+{
+    public:
+    // Ich bin mir nicht sicher, wo public oder private sinnvoller ist.
+        double x_base = 0, y_base = 0, z_base = 0;
+        double height;
+        int branches = 0;
+};
